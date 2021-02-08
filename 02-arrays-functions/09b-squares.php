c7372f82d2603be2e97c4e2b852ecbbd42834e40
Kate Fox <katefox100@gmail.com>
2021-02-08 17:14:44 +0000

    09b Squares challenge using collect() complete

---
+++ 02-arrays-functions/09b-squares.php
@@ -0,0 +1,19 @@
+<?php
+
+// Create a function, squares, that takes an array of numbers as an argument. Return a new array containing the square of each number.
+// Try it using foreach
+// Try it using collect() (you'll need the illuminate/support package)
+
+require __DIR__ . "/vendor/autoload.php";
+
+function squares($values) {
+
+    $squares = collect($values)->map(fn($value) => $value * $value);
+
+    return $squares->all();
+}
+
+dump(
+    squares([2, 3, 4]), // [4, 9, 16]
+    squares([2, 3, 4, 5, 6]), // [4, 9, 16, 25, 36]
+);
