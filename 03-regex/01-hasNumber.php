19fa8eafccbc0f1454fac2b27bec382da7c7ff4e
Kate Fox <katefox100@gmail.com>
2021-02-09 12:15:46 +0000

    01-hasNumber challenge complete, also new composer files

---
+++ 03-regex/01-hasNumber.php
@@ -0,0 +1,13 @@
+<?php
+
+// Create a function hasNumber, that returns true if the given string contains a digit.
+
+require __DIR__ . "/vendor/autoload.php";
+
+function hasNumber($str) {
+    return preg_match("/\d/", $str) === 1;
+}
+
+dump(hasNumber("blah")); // false
+dump(hasNumber("bl3h")); // true
+dump(hasNumber("bl3h5")); // true
