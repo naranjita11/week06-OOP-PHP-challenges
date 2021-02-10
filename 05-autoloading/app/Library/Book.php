4f253a6d3e9b9e2be378e0febe5bbbd469b91032
Kate Fox <katefox100@gmail.com>
2021-02-10 14:24:45 +0000

    03 Book challenge complete

---
+++ 05-autoloading/app/Library/Book.php
@@ -0,0 +1,29 @@
+<?php
+
+// Create a class Book in the App\Library namespace. It should take a title and its number of pages in the constructor. It should have a read() method, which takes a number of pages that have been read. It should also have a currentPage() page method which tells you which page you're currently on. Put the following in index.php and then check that you get back the right results:
+
+namespace App\Library;
+
+class Book
+{
+    private $title;
+    private $noOfPages;
+    private $currentPage = 1;
+
+    public function __construct($titleArg, $noOfPagesArg)
+    {
+        $this->title = $titleArg;
+        $this->noOfPages = $noOfPagesArg;
+    }
+
+    public function read($pagesReadArg)
+    {
+        $this->currentPage += $pagesReadArg;
+    }
+
+    public function currentPage()
+    {
+        return $this->currentPage;
+    }
+
+}
