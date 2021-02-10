9414efd0248747d14f953b5e9b8f9686a5f00d5d
Kate Fox <katefox100@gmail.com>
2021-02-10 12:45:11 +0000

    01-Hello complete

--- 05-autoloading/index.php
+++ 05-autoloading/index.php
@@ -2,3 +2,11 @@
 
 require __DIR__ . "/vendor/autoload.php";
 
+echo "\nQuestion 1:\n";
+
+use App\Hello;
+
+$sayHi = new Hello();
+
+dump($sayHi->hello("Orb")); // "Hello Orb"
+dump($sayHi->hello("Horse")); // "Hello Horse"
